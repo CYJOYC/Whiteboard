6ce43b91f1719b6f099b7bce70087d1319665d30
Ryan Qiao <mengyuan.q@berkeley.edu>
2020-11-28 13:19:44 -0800

    initial setup for comments

---
+++ nextjs-whiteboard/pages/comments/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import SingleComment from '../../components/Comments/SingleComment.js';
+
+export default function Project() {
+  return <React.StrictMode>
+      <SingleComment/>
+  </React.StrictMode>
+}
+
