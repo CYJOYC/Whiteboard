c537acf5416ea340ae7ed1a0b3cd1ac72053cb9a
William Zhang <williamyixin@berkeley.edu>
2020-11-28 16:35:52 -0800

    basic functionality with test room nums completed

--- nextjs-whiteboard/components/Gallery.js
+++ nextjs-whiteboard/components/Gallery.js
@@ -1,26 +1,89 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { auth, db } from '../config/firebase';
 
-class Gallery extends React.Component {
-  renderImage(imageUrl) {
-    console.log("alkjfe type of imageURL: " + typeof(imageUrl) + imageUrl)
+function Gallery() {
+
+  const [imageURLs, setimageURLs] = useState([]);
+
+
+  var picturesRef = db.ref("galleries/" + "0000" + "/pictures");
+  
+   let json = {}
+   picturesRef.on("value", function(snapshot) {
+      json = snapshot.val();
+
+      let test = []
+      for (let element in json) {
+        test.push(json[element])
+      }
+      
+      if (!arraysEqual(test, imageURLs)) {
+        setimageURLs(test)
+      }
+        // for (let element in json) {
+        //   if (!(imageURLs.includes(json[element]['imageURL']))) {
+        //     let arrayClone = [...imageURLs]
+        //     setimageURLs(arrayClone.push(json[element]['imageURL']))
+        //   }
+        // }
+  }, function (errorObject) {
+    console.log("The read failed: " + errorObject.code);
+  });  
+
+  // let imageURLs = ['https://homepages.cae.wisc.edu/~ece533/images/fruits.png','https://homepages.cae.wisc.edu/~ece533/images/peppers.png']
+  
+  function arraysEqual(a1,a2) {
+    /* WARNING: arrays must not contain {objects} or behavior may be undefined */
+    return JSON.stringify(a1)==JSON.stringify(a2);
+  }
+
+  function renderImage(imageUrl) {
     return (
       <div>
-        <img src={imageUrl} />
+        <img src={imageUrl['imageURL']} />
+        <figcaption>Created by {imageUrl['creator']}</figcaption>
       </div>
     );
   }
 
-  render() {
-    return (
-      <div className="gallery">
-        <div className="images">
-          {this.props.imageUrls.map(imageUrl => this.renderImage(imageUrl))}
-        </div>
+
+  
+  return (
+    <div className="gallery">
+      <div className="images">
+        {imageURLs.map(imageUrl => renderImage(imageUrl))}
       </div>
-    );
-  }
+    </div>
+  );
+  
+
 }
-// Gallery.propTypes = {
-//   imageUrls: React.PropTypes.arrayOf(React.PropTypes.string).isRequired
-// };
+
 export default Gallery;
+
+// import React from "react";
+
+// class Gallery extends React.Component {
+//   renderImage(imageUrl) {
+//     console.log("alkjfe type of imageURL: " + typeof(imageUrl) + imageUrl)
+//     return (
+//       <div>
+//         <img src={imageUrl} />
+//       </div>
+//     );
+//   }
+
+//   render() {
+//     return (
+//       <div className="gallery">
+//         <div className="images">
+//           {this.props.imageUrls.map(imageUrl => this.renderImage(imageUrl))}
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+// // Gallery.propTypes = {
+// //   imageUrls: React.PropTypes.arrayOf(React.PropTypes.string).isRequired
+// // };
+// export default Gallery;
--- nextjs-whiteboard/pages/gallery/index.js
+++ nextjs-whiteboard/pages/gallery/index.js
@@ -7,25 +7,12 @@ import Gallery from '../../components/Gallery.js';
 
 export default function Project() {
   
-  let data = []; 
-  var picturesRef = db.ref("galleries/" + "0000" + "/pictures");
-  
-  let json = {}
-  picturesRef.on("value", function(snapshot) {
-    console.log(snapshot.val());
-    json = snapshot.val();
-  }, function (errorObject) {
-    console.log("The read failed: " + errorObject.code);
-  });  
 
-  for (let element in json) {
-    data.push(json[element]['imageURL'])
-  }
+
   
-  console.log(data)
 
   return <React.StrictMode>
-    <Gallery imageUrls={data}/>
+    <Gallery/>
   </React.StrictMode>
 }
 
