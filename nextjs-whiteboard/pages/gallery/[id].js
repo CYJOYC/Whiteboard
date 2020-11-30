f4a2600c23c1f719477f287011d84f72e79b5b14
Jacqueline Zhang <jackiegzhang@gmail.com>
2020-11-30 02:26:01 -0800

    using getserversideprops instead

--- nextjs-whiteboard/pages/gallery/[id].js
+++ nextjs-whiteboard/pages/gallery/[id].js
@@ -12,24 +12,24 @@ export default function Project(props) {
 	</React.StrictMode>
 }
 
-// This function gets called at build time
-export async function getStaticPaths() {
-	// Call an external API endpoint to get posts
-	let galleries = null;
-	await db.ref(`galleries/`).once('value', (snapshot) => {
-	const data = snapshot.val();
-		galleries = Object.keys(data);
-	})
+// // This function gets called at build time
+// export async function getStaticPaths() {
+// 	// Call an external API endpoint to get posts
+// 	let galleries = null;
+// 	await db.ref(`galleries/`).once('value', (snapshot) => {
+// 	const data = snapshot.val();
+// 		galleries = Object.keys(data);
+// 	})
 
-	// Get the paths we want to pre-render based on posts
-	const paths = galleries.map((galleryId) => `/gallery/${galleryId}`)
+// 	// Get the paths we want to pre-render based on posts
+// 	const paths = galleries.map((galleryId) => `/gallery/${galleryId}`)
 
-	// We'll pre-render only these paths at build time.
-	// { fallback: false } means other routes should 404.
-	return { paths, fallback: false }
-}
+// 	// We'll pre-render only these paths at build time.
+// 	// { fallback: false } means other routes should 404.
+// 	return { paths, fallback: false }
+// }
 
-export async function getStaticProps(context) {
+export async function getServerSideProps(context) {
 	const galleryCode = context.params.id
 	let data = null;
 	await db.ref(`galleries/${galleryCode}/`).once('value', (snapshot) => {
