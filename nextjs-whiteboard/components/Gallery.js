ab60f2422f252d73d54e905718938720600b66de
Jacqueline Zhang <jackiegzhang@gmail.com>
2020-11-29 21:00:21 -0800

    fixed router error and added dashboard button

--- nextjs-whiteboard/components/Gallery.js
+++ nextjs-whiteboard/components/Gallery.js
@@ -4,23 +4,29 @@ import Button from '../components/button';
 import { useRouter } from 'next/router';
 import { useGallery } from '../hooks/useGallery';
 import Loader from '../components/loader';
+import Modal from 'react-modal';
+import CommentsBlock from './Comments/CommentsBlock.js';
+import { useAuth } from '.././hooks/useAuth';
 
 function arraysEqual(a1,a2) {
   return JSON.stringify(a1)==JSON.stringify(a2);
 }
 
 function Gallery(props) {
+  const auth = useAuth();
   const [imageURLs, setimageURLs] = useState([]);
-  // const router = useRouter();
+  const router = useRouter();
   // const gallery = useGallery();
   // if (!gallery.gallery ) return <Loader />;
   // const galleryCode = router.query.id;
   let galleryName = props.data.name;
-  let picturesTemp = []
+  let picturesTemp = [];
   for (let element in props.data.pictures) {
-    picturesTemp.push(props.data.pictures[element])
+    let image = props.data.pictures[element];
+    image['picId'] = element;
+    picturesTemp.push(image)
   }
-  
+
   if (!arraysEqual(picturesTemp, imageURLs)) {
     setimageURLs(picturesTemp)
   }
@@ -28,11 +34,13 @@ function Gallery(props) {
   function renderImage(imageUrl) {
     return (
       <div className={styles.card}>
-        <img src={imageUrl['imageURL']} />
+        <img src={imageUrl['imageURL']}/>
         <figcaption>Created by {imageUrl['creator']}</figcaption>
+        <CommentsBlock comments={imageUrl['comments']} user={auth.user.name} picId={imageUrl['picId']}/>
       </div>
     );
   }
+
 
   function createBoard() {
     router.push({
@@ -41,10 +49,17 @@ function Gallery(props) {
     });
   }
 
+  function backToDashboard() {
+    router.push('/dashboard');
+  }
+
   
-  
+  if (!auth.user || !auth.user.name) return <Loader />;
   return (
     <div>
+      <div className={styles.backToDashboardBtn}>
+        <Button type={'outline'} name={'Back to Dashboard'} onClick={backToDashboard} />
+      </div>
       <div className={styles.title}>
         Gallery Room {galleryName}
       </div>
@@ -53,7 +68,7 @@ function Gallery(props) {
       </div>
       <div className={styles.gallery}>
         <div className={styles.images}>
-          {imageURLs.map(imageUrl => renderImage(imageUrl))}
+          {imageURLs.map((imageUrl) => renderImage(imageUrl))}
         </div>
       </div>
     </div>
