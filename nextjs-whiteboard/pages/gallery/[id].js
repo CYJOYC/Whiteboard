e91314c273ebbbd51180baff003e3b7e9ccd4028
Jacqueline Zhang <jackiegzhang@gmail.com>
2020-12-03 01:00:33 -0800

    added learnings.md file

--- nextjs-whiteboard/pages/gallery/[id].js
+++ nextjs-whiteboard/pages/gallery/[id].js
@@ -1,12 +1,12 @@
 import React from 'react';
 //import './index.css';
 import Gallery from '../../components/Gallery.js';
-import { useAuth } from '../../hooks/useAuth';
+import { useRequireAuth } from '../../hooks/useRequireAuth';
 import { db } from '../../config/firebase';
 import Loader from '../../components/Loader'; 
 
 export default function Project(props) {
-	const auth = useAuth();
+	const auth = useRequireAuth();
 	return <React.StrictMode>
 		<Gallery data={props.data} user={auth.user} code={props.galleryCode}/>
 	</React.StrictMode>
